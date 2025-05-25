ab81fd75238364fb629eb31e99b0114d1544f267
Ravisha Jain <40623176+RavishaJain@users.noreply.github.com>
2025-05-25 12:58:23 +0000

    hw-2

--- 02-experiment-tracking/homework/train.py
+++ 02-experiment-tracking/homework/train.py
@@ -34,7 +34,7 @@ def run(data_path):
     rf.fit(X_train, y_train)
     y_pred = rf.predict(X_valid)
 
-    rmse = mean_squared_error(y_valid, y_pred, squared=False)
+     rmse = mean_squared_error(y_val, y_pred) ** 0.5
 
 
 if __name__ == '__main__':
